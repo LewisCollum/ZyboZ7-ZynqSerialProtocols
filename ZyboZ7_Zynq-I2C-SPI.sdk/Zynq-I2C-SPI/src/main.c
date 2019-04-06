d8065861dc3009779cd4e97133e7705083db3711
LewisCollum <lewiscollum4@gmail.com>
2019-04-06 18:19:49 -0400

    Extract function refactored pcf8591-related code.

--- ZyboZ7_Zynq-I2C-SPI.sdk/Zynq-I2C-SPI/src/main.c
+++ ZyboZ7_Zynq-I2C-SPI.sdk/Zynq-I2C-SPI/src/main.c
@@ -1,44 +1,32 @@
 #include "xparameters.h"
 #include "xiicps.h"
+#include "adc_pcf8591.h"
 
 #define i2cControllerDeviceId XPAR_XIICPS_0_DEVICE_ID
 #define i2cSlaveAddress 0x48
-#define i2cSlaveClockRate 100000
-#define PCF8591_DAC_ENABLE 0x40
-#define PCF8591_ADC_CH0 0x40
-#define PCF8591_ADC_CH1 0x41
-#define PCF8591_ADC_CH2 0x42
-#define PCF8591_ADC_CH3 0x43
 
-const uint8_t adcChannelPotentiometer = 0x03;
+enum YML40Channel {
+	photoresistor, thermistor, custom, potentiometer
+} channel;
 
-int IicPsMasterPolledExample(u16 DeviceId);
-
-XIicPs i2cController;
-uint8_t adcSample;
-
-enum State{
+enum State {
 	waiting, sending, fetching
 } state;
 
+XIicPs i2cController;
+unsigned char adcSample;
+
 
 void setup() {
-	XIicPs_Config* Config;
-	Config = XIicPs_LookupConfig(i2cControllerDeviceId);
-	XIicPs_CfgInitialize(&i2cController, Config, Config->BaseAddress);
-	XIicPs_SelfTest(&i2cController);
-	XIicPs_SetSClk(&i2cController, i2cSlaveClockRate);
-
-	XIicPs_MasterSendPolled(&i2cController, (uint8_t*) &adcChannelPotentiometer, 1, i2cSlaveAddress);
-	while (XIicPs_BusIsBusy(&i2cController));
+	pcfADC_setupWithBaseAddress();
+	pcfADC_setChannel(potentiometer);
 }
 
 void run() {
-	XIicPs_MasterRecvPolled(&i2cController, (uint8_t*)&adcSample, 1, i2cSlaveAddress);
+	adcSample = pcfADC_getSample();
 }
 
-int main(void)
-{
+int main() {
   setup();
   while(1) run();
   return 0;
